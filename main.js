56ae36acd7758d00f0d0523c77ff6cc5232f00d8
jktham-pc <jonas.tham@gmx.ch>
2021-09-14 22:52:39 +0200

    Add form submit functionality, display book info

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 const shelf = document.querySelector("#shelf");
-const addBookButton = document.querySelector("#add-book");
+const addBookButton = document.querySelector("#button-add");
 const titleInput = document.querySelector("#input-title");
 const authorInput = document.querySelector("#input-author");
 const pagesInput = document.querySelector("#input-pages");
 const readInput = document.querySelector("#input-read");
+const inputForm = document.querySelector("#input-form");
 
 let library = [];
 
-addBookButton.addEventListener("click", () => {
-    addBook(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
-    titleInput.value = "";
-    authorInput.value = "";
-    pagesInput.value = "";
-    readInput.checked = false;
-});
+function submitForm() {
+    if (inputForm.checkValidity()) {
+        addBook(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
+        drawBooks(library);
+        inputForm.reset();
+    }
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -25,9 +26,32 @@ function Book(title, author, pages, read) {
 function addBook(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     library.push(book);
+}
 
-    let bookDiv = document.createElement("div");
-    bookDiv.id = "book-div";
+function drawBooks(lib) {
+    shelf.innerHTML = "";
+    for (let i=0; i<lib.length; i++) {
+        let book = lib[i];
 
-    shelf.appendChild(bookDiv);
+        let bookDiv = document.createElement("div");
+        bookDiv.id = "book-div";
+        let bookTitle = document.createElement("span");
+        bookTitle.textContent = book.title;
+        bookTitle.id = "book-title"
+        let bookAuthor = document.createElement("span");
+        bookAuthor.textContent = book.author;
+        bookAuthor.id = "book-author"
+        let bookPages = document.createElement("span");
+        bookPages.textContent = book.pages + " pages";
+        bookPages.id = "book-pages"
+        let bookRead = document.createElement("span");
+        bookRead.textContent = book.read ? "Read" : "Not Read";
+        bookRead.id = "book-read"
+
+        shelf.appendChild(bookDiv);
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(bookPages);
+        bookDiv.appendChild(bookRead);
+    }
 }
