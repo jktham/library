58c64c71670e8aba1e1fd66c557fe37dc1a46956
jktham-pc <jonas.tham@gmx.ch>
2021-09-15 00:13:06 +0200

    Add random books, button to reset library

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 const shelf = document.querySelector("#shelf");
-const addBookButton = document.querySelector("#button-add");
 const titleInput = document.querySelector("#input-title");
 const authorInput = document.querySelector("#input-author");
 const pagesInput = document.querySelector("#input-pages");
 const readInput = document.querySelector("#input-read");
 const inputForm = document.querySelector("#input-form");
+const randomBookButton = document.querySelector("#random-book");
+const deleteLibraryButton = document.querySelector("#delete-library");
 
 let library = [];
 
-function submitForm() {
+inputForm.addEventListener("submit", () => {
     if (inputForm.checkValidity()) {
         addBook(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
         drawBooks(library);
         inputForm.reset();
     }
-}
+});
+
+randomBookButton.addEventListener("click", () => {
+    let randomTitles = ["A Game of Thrones", "Harry Potter and the Chamber of Secrets", "To Kill a Mockingbird", "The Great Gatsby", "Frankenstein", "Faust"];
+    let randomAuthors = ["George R. R. Martin", "J. K. Rowling", "Harper Lee", "F. Scott Fitzgerald", "Mary Shelley", "Johann Wolfgang von Goethe"];
+    let randomTitle = randomTitles[Math.floor(Math.random()*randomTitles.length)];
+    let randomAuthor = randomAuthors[Math.floor(Math.random()*randomAuthors.length)];
+    let randomPage = Math.floor(Math.random()*1000);
+    let randomRead = Math.random() < 0.5;
+    addBook(randomTitle, randomAuthor, randomPage, randomRead);
+    drawBooks(library);
+});
+
+deleteLibraryButton.addEventListener("click", () => {
+    library = [];
+    drawBooks(library);
+});
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -33,8 +51,8 @@ function drawBooks(lib) {
     for (let i=0; i<lib.length; i++) {
         let book = lib[i];
 
-        let bookDiv = document.createElement("div");
-        bookDiv.id = "book-div";
+        let bookCard = document.createElement("div");
+        bookCard.id = "book-card";
         let bookTitle = document.createElement("span");
         bookTitle.textContent = book.title;
         bookTitle.id = "book-title"
@@ -48,10 +66,10 @@ function drawBooks(lib) {
         bookRead.textContent = book.read ? "Read" : "Not Read";
         bookRead.id = "book-read"
 
-        shelf.appendChild(bookDiv);
-        bookDiv.appendChild(bookTitle);
-        bookDiv.appendChild(bookAuthor);
-        bookDiv.appendChild(bookPages);
-        bookDiv.appendChild(bookRead);
+        shelf.appendChild(bookCard);
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookRead);
     }
 }
